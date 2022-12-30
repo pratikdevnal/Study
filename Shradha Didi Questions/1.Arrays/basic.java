4aa99b69bc5637619c1262757bad88c819a61348
Pratik <pratik.devnal@gmail.com>
2022-12-30 21:01:54 +0530

    edited basic array

--- Shradha Didi Questions/1.Arrays/basic.java
+++ Shradha Didi Questions/1.Arrays/basic.java
@@ -1,6 +1,13 @@
+import java.util.*;
+
 public class basic {
     public static void main(String[] args) {
         int a[] = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            a[i] = sc.nextInt();
+        }
         for (int i = 0; i < 10; i++) {
             System.out.println(a[i]);
         }
