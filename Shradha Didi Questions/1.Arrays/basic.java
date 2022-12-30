4875ed908629fe8bd197ac9673648f4192fa8aff
Pratik <pratik.devnal@gmail.com>
2022-12-30 20:58:33 +0530

    array started

---
+++ Shradha Didi Questions/1.Arrays/basic.java
@@ -0,0 +1,9 @@
+public class basic {
+    public static void main(String[] args) {
+        int a[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a[i]);
+        }
+        System.out.println("arrays");
+    }
+}
