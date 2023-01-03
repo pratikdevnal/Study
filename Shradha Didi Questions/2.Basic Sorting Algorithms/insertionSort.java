196b6dccb917f94f79a806c949ba31e32b06ef42
Pratik <pratik.devnal@gmail.com>
2023-01-03 19:46:59 +0530

    insertionSort.java

---
+++ Shradha Didi Questions/2.Basic Sorting Algorithms/insertionSort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class insertionSort {
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > current) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+
+            }
+            arr[prev + 1] = current;
+
+        }
+    }
+}
