642d8ac7b75f29fe09e56071bddcf6596792f5ae
Pratik <pratik.devnal@gmail.com>
2023-01-06 21:58:26 +0530

    diagonalSum.java

---
+++ Shradha Didi Questions/3.2D-Arrays/diagonalSum.java
@@ -0,0 +1,27 @@
+public class diagonalSum {
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+
+        System.out.println(sum(matrix));
+    }
+
+    /**
+     * @param matrix
+     * @return
+     */
+    public static int sum(int matrix[][]) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            // primary diagonal
+            sum += matrix[i][i];
+            // secondary diagonal
+            if (i != matrix.length - i - 1) {
+                sum += matrix[i][matrix.length - i - 1];
+            }
+        }
+        return sum;
+    }
+}
